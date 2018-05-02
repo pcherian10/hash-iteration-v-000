af714b1b39d4294d5e880d071f4ecc4cc8a73cec
Paul <pcherian10@gmail.com>
2018-05-02 18:13:44 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,16 +1,21 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
+birthday_kids = {
+ "Timmy" => 9,
+ "Sarah" => 6,
+ "Amanda" => 27
+}
+
 def happy_birthday(birthday_kids)
-  birthday_kids = {
-   "Timmy" => 9, 
-   "Sarah" => 6, 
-   "Amanda" => 27
- }
+
+birthday_kids 
+
+
+
+ 
 
 end
-
-
